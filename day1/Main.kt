2fdad52de3215803b605fa8647ea49513e5001ac
Timmy Nielsen <timmy.j.nielsen@gmail.com>
2024-12-03 23:39:28 +0100

    Puzzle 2

--- day1/Main.kt
+++ day1/Main.kt
@@ -1,7 +1,7 @@
-package Day1
+package day1
 
 fun main() {
-    val resource = object {}.javaClass.getResource("./data/input") // Data path to input data
+    val resource = object {}.javaClass.getResource("./puzzle1/data/input") // Data path to input data
         ?: error("File not found")
 
     val bufferedReader = resource.openStream().bufferedReader()
@@ -28,11 +28,25 @@ fun main() {
         }
     }
 
-    var finalDiff: Int = 0
+    var puzzle2FinalDiff: Int = 0
+
+    // Iterate through the first list
+    firstList.forEach { (key, value) -> 
+        // This is what the puzzle asks for from each entry in the first list
+        val keyTimesOccurrencesInSecondList = key * (secondList[key] ?: 0)
+
+        // Since the hashmap tells us how many times the key will appear in the first list, we can add all the occurences together by multiplying by the value in the hashmap right away
+        val keyTimesOccurrencesInSecondListTimesOccurencesInFirstList = keyTimesOccurrencesInSecondList * value
+
+        // Then we add them all together for the result
+        puzzle2FinalDiff += keyTimesOccurrencesInSecondListTimesOccurencesInFirstList
+    }
+
+    var puzzle1FinalDiff: Int = 0
 
     // Iterate through the first list in ascending key order
     firstList.forEach { (key, _) ->
-        // Add the diff until 0 occurrences left for each key
+        // Add the diff until 0 occurrences left in first list (and second since they're the same size)
         while ((firstList[key] ?: 0) > 0) {
             var secondListFirstKey = secondList.firstKey() // First key always matches the lowest value in second list
             if (secondList[secondListFirstKey] == 0) {
@@ -43,7 +57,7 @@ fun main() {
             secondListFirstKey = secondList.firstKey() // Reinitialize in case key was removed
 
             // Add to final diff
-            finalDiff += kotlin.math.abs(key - secondListFirstKey)
+            puzzle1FinalDiff += kotlin.math.abs(key - secondListFirstKey)
 
             // Subtract the used key for next loop
             secondList[secondListFirstKey] = (secondList[secondListFirstKey] ?: 0) - 1
@@ -51,7 +65,7 @@ fun main() {
         }
     }
 
-    println("Puzzle 1 final diff: $finalDiff")
-
+    println("Puzzle 1 final diff: $puzzle1FinalDiff")
+    println("Puzzle 2 final diff: $puzzle2FinalDiff")
 
 }
